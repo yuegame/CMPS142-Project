cca56cf86c9f53e5b2beaa2a166dc5db1550125b
Rohit Falor <rohitfalor@outlook.com>
2018-12-05 00:01:45 -0800

    Updated split

--- split.py
+++ split.py
@@ -1,4 +1,5 @@
 def tokenize(s):
+    last_punct = False
     combined = []
     words = s.split(' ')
     i = 0
@@ -7,10 +8,14 @@ def tokenize(s):
             if(not words[i+1][0].isalpha()):
                 combined.append(words[i] + words[i + 1])
                 i += 1
+
+                if i == len(words) - 1:
+                    last_punct = True
             else:
                 combined.append(words[i])
         i += 1
     
-    combined.append(words[i])
+    if not last_punct:
+        combined.append(words[i])
 
     return combined
