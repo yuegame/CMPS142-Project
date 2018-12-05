d6f7f7baa0d5d9e6d1fb2f1a70a962c5de20a498
Rohit Falor <rohitfalor@outlook.com>
2018-12-05 14:20:22 -0800

    update

--- bayes.py
+++ bayes.py
@@ -1,29 +1,21 @@
-import csv, sys, math, random, operator, copy
-import nltk, split
+import csv, sys, math, random, operator
+import split
 
 train_nodes = []
 
-def get_all_words(train_nodes):
-    w = set()
-    for n in train_nodes:
-        (phrase_id, sentence_id, phrase, label) = n
-        w.update(split.tokenize(phrase))
-    return w
+def get_features(train_nodes):
+    f = {}
 
-def get_features(all_words, train_nodes):
+def get_features(train_nodes):
     f = []
     phrase_features = {word: 0 for word in all_words}
     for node in train_nodes:
-        (phrase_id, sentence_id, phrase, label) = node
-        phrase_words = split.tokenize(phrase)
-        for word in phrase_words: 
-            phrase_features[word] = 1  
+        phrase, label = node[-2], node[-1]
+        words = split.tokenize(phrase)
 
-        f.append((copy.deepcopy(phrase_features), label))
-
-        # reset dict
-        for word in phrase_words: 
-            phrase_features[word] = 0
+        for word in words:
+            f[word] = label
+    
     return f
 
     
@@ -40,10 +32,7 @@ if __name__ == "__main__":
                 train_nodes.append(row)
 
     print(train_nodes[:5])
-    all_words = get_all_words(train_nodes)
-    train_set = get_features(all_words, train_nodes)
+    train_set = get_features(train_nodes)
     print(train_set[0])
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
-    prediction = classifier.classify({word:1 for word in split.tokenize(', inane images keep popping past your head')})
-    print(prediction)
+
 
